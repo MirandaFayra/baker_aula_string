4095c436c1a26837593add49d6c705447af9f2ff
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-08-30 19:19:58 -0300

    Finalizar estruturas dos exercicios

---
+++ script.js
@@ -0,0 +1,13 @@
+// 4 modos diferentes de declarar funcoes 
+// Função => trechos de código reutilizáveis 
+// Parametros tá dentro do () quando definimos função 
+// Argumentos tá dentro do () quando chamaos a função 
+
+//------------- EXERCICIO 1 ------------ 
+
+
+//------------- EXERCICIO 1 ------------ 
+
+//------------- EXERCICIO 2 ------------
+
+//------------- EXERCICIO 3 ------------
