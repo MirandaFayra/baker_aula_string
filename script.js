b3b9998d41700abba0ea7b1c25fa1e59c9df53e8
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-08-30 19:34:25 -0300

    Finalizar a base dos exemplos

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-// 4 modos diferentes de declarar funcoes 
+// 4 modos diferentes de declarar função 
 // Função => trechos de código reutilizáveis 
 // Parametros tá dentro do () quando definimos função 
 // Argumentos tá dentro do () quando chamaos a função 
 
-//------------- EXERCICIO 1 ------------ 
+//--------------- RELEMBRANDO --------------
+
+// DECLARANDO STRING => " " | ' ' | `String desejada ${variável} `
+
+let stringDeExemplo = 'Salveeee Baker, boa noite Baker! '
+
+// USANDO " " DENTRO DA STRING => "Ela disse: \"Olá!\""
+
+// A. VERIFICAR TAMANHO =>  string.length
+// B. STRING MAIUSCULA => string.toUpperCase()
+// C. STRING MINUSCULA => string.toLowerCase()
+// D. REMOVER ESPAÇOS EM BRANCO => string.trim()
+// E. SUBSTITUIR UMA POR OUTRA => 1ª  OCORRÊNCIA =>
+//string.replace("string atual", "string para subsutituir")
+// F. SUBSTITUIR UMA POR OUTRA => TODAS AS OCORRÊNCIA => 
+// string.replaceAll("string atual", "string para subsutituir")
+// G. VERIFICAR SE TEM O TERMO => string.includes("Termo a ser verificado")
+// H. CORTAR STRING =>  string.split(separador, limite)
 
 
-//------------- EXERCICIO 1 ------------ 
 
-//------------- EXERCICIO 2 ------------
+//---------------- EXERCICIO 1 ------------- 
 
-//------------- EXERCICIO 3 ------------
+
+//---------------- EXERCICIO 2 ------------- 
+
+//---------------- EXERCICIO 3 -------------
+
+//---------------- EXERCICIO 3 -------------
