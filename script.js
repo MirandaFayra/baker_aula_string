4d5c62c79d8df924edc7559f65a74a6389a8a09e
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-08-30 20:04:46 -0300

    Finalizar o exemplo de explicacao

--- script.js
+++ script.js
@@ -7,21 +7,38 @@
 
 // DECLARANDO STRING => " " | ' ' | `String desejada ${variável} `
 
-let stringDeExemplo = 'Salveeee Baker, boa noite Baker! '
+let stringDeExemplo = 'Salveeee Baker, boa noite Baker!     '
 
 // USANDO " " DENTRO DA STRING => "Ela disse: \"Olá!\""
 
 // A. VERIFICAR TAMANHO =>  string.length
+//console.log(`Esse é o tamanho da string ${stringDeExemplo.length} caracteres`)
+
 // B. STRING MAIUSCULA => string.toUpperCase()
+//console.log(`Esse é a  string maiúscula ${stringDeExemplo.toUpperCase()}`)
+
 // C. STRING MINUSCULA => string.toLowerCase()
+//console.log(`Esse é a  string minúscula ${stringDeExemplo.toLowerCase()}`)
+
 // D. REMOVER ESPAÇOS EM BRANCO => string.trim()
+//console.log(`Esse é a  string sem espaço no final ${stringDeExemplo.trim()}`)
+
 // E. SUBSTITUIR UMA POR OUTRA => 1ª  OCORRÊNCIA =>
 //string.replace("string atual", "string para subsutituir")
+//console.log(`Esse é a string substituida a primeira ocorrência ${stringDeExemplo.replace(" Baker", " bananinha")}`)
+
+
 // F. SUBSTITUIR UMA POR OUTRA => TODAS AS OCORRÊNCIA => 
 // string.replaceAll("string atual", "string para subsutituir")
+//console.log(`Esse é a string substituida em todas as ocorrências ${stringDeExemplo.replaceAll(" Baker", " bananinha")}`)
+
 // G. VERIFICAR SE TEM O TERMO => string.includes("Termo a ser verificado")
+
+//console.log(`Esse é a string que vamos verificar ${stringDeExemplo.includes("ake")}`)
+
 // H. CORTAR STRING =>  string.split(separador, limite)
 
+//console.log(`Esse é a string que vamos cortar ${stringDeExemplo.split(" ")}`)
 
 
 //---------------- EXERCICIO 1 ------------- 
